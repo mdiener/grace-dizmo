c2668cc16e596d7a0971ca4d3e4be4fd085e71bc
Michael Diener <dm.menthos@gmail.com>
2013-06-10 17:57:44 -0700

    Skeleton
    added some comments to application.js file
    
    Signed-off-by: Michael Diener <dm.menthos@gmail.com>

--- grace-dizmo/skeleton/dizmo/src/application_X.js
+++ grace-dizmo/skeleton/dizmo/src/application_X.js
@@ -1,5 +1,10 @@
 //= require Main
 
+/*
+Generally you do not need to edit this file. You can start writing
+your code in the provided "Main" class.
+*/
+
 // Needed for the dizmo menu to work
 function showBack() {
     dizmo.showBack();
@@ -13,6 +18,7 @@ function showFront() {
 // Helper object to attach all the events to
 var events = {};
 
+// As soon as the dom is loaded, call instantiate the main class
 jQuery(document).ready(function() {
     new #PROJECTNAME.Main();
 });
