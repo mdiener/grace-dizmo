8981b41ed806d5ec0e528ade57ce3cad75f49eb4
Michael Diener <dm.menthos@gmail.com>
2013-09-29 20:40:50 -0700

    Plugin - build
    Added the option to use an svg as icon for the dizmo
    
    Signed-off-by: Michael Diener <dm.menthos@gmail.com>

--- grace-dizmo/plugin.py
+++ grace-dizmo/plugin.py
@@ -107,14 +107,18 @@ class Dizmo:
     def after_build(self):
         plist = self._get_plist()
         path = self._config['build_path']
+        source = os.path.join(os.getcwd(), 'Icon.png')
 
         try:
             plistlib.writePlist(plist, os.path.join(path, 'Info.plist'))
         except:
             raise FileNotWritableError('Could not write plist to target location: ', path)
 
+        if os.path.isfile(os.path.join(os.getcwd(), 'Icon.svg')):
+            source = os.path.join(os.getcwd(), 'Icon.svg')
+
         try:
-            copy(os.path.join(os.getcwd(), 'Icon.png'), os.path.join(path, 'Icon.png'))
+            copy(source, os.path.join(path, 'Icon.png'))
         except:
             print 'Could not find an icon for your dizmo. You should consider placing a `Icon.png` in your root folder.'
 
