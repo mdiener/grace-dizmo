9211948a81b140e9f41ec451ec3a4b4d9c2f14b0
Michael Diener <dm.menthos@gmail.com>
2014-04-10 22:09:11 -0700

    Skeleton
    * more sophisticated test setup
    
    Signed-off-by: Michael Diener <dm.menthos@gmail.com>

---
+++ grace-dizmo/skeleton/dizmo/test/javascript/test_dizmo_X.js
@@ -0,0 +1,6 @@
+module('dizmo tests');
+test('test case one', function(assert) {
+    // do testing here.
+    // refer to the QUnit documentation for more information
+    // https://qunitjs.com/
+});
---
+++ grace-dizmo/skeleton/dizmo/test/test_X.js
@@ -0,0 +1,11 @@
+//= require test_dizmo
+
+QUnit.begin(function() {
+    // setup before running tests
+});
+
+QUnit.done(function() {
+    // things to be done once tests are run
+    dizmo.setAttribute('geometry/width', 1000);
+    dizmo.setAttribute('geometry/height', 490);
+});
