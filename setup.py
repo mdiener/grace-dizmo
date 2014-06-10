3acc6dce9048c05b6d7df299794f76d9d2c5d2fc
Michael Diener <dm.menthos@gmail.com>
2014-06-09 19:39:29 -0700

    increased version
    Signed-off-by: Michael Diener <dm.menthos@gmail.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author='Michael Diener',
     author_email='michael@webdiener.ch',
     url='https://github.com/mdiener/grace-dizmo',
-    version='0.1.52',
+    version='0.1.53',
     license='LICENSE.txt',
     packages=['grace-dizmo'],
     install_requires=['grace', 'setuptools'],
