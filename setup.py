3b4fed2e8fa6eaf70d2ddea95ebc62c1c86ac2e4
Michael Diener <dm.menthos@gmail.com>
2014-07-02 11:29:47 -0700

    increased version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author='Michael Diener',
     author_email='michael@webdiener.ch',
     url='https://github.com/mdiener/grace-dizmo',
-    version='0.1.57',
+    version='0.1.58',
     license='LICENSE.txt',
     packages=['grace-dizmo'],
     install_requires=['grace', 'setuptools'],
